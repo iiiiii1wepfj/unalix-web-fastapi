e22db2313ac9cc36b24236f5c9df3594e6f1c9bc
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-07-19 16:26:58 +0300

    test

--- main.py
+++ main.py
@@ -22,12 +22,7 @@ import re
 import sys
 
 
-app = FastAPI(
-    docs_url=None,
-    title=app_title,
-    description=app_description,
-    version=app_version,
-)
+app = FastAPI(docs_url=None, title=app_title, description=app_description, version=app_version)
 
 templates = Jinja2Templates(directory="templates")
 
