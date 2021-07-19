c4b6496a039682cf0a661ad5f5b83b5c481fee4b
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-07-19 10:58:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
-port = "8000"
+port_str = "8000"
 
-port = int(port)
+port = int(port_str)
 
 host = "0.0.0.0"
+
+app_title = "Unalix-web"
+
+app_description = "source code: https://github.com/AmanoTeam/unalix-web-fastapi."
+
+app_version = "2.0"
 
 show_server_errors = False
