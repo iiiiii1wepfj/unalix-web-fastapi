4d7dfdc0cb1cf50c429db7140702c1450259eba6
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-07-30 11:47:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,11 @@ app_title = "Unalix-web"
 
 app_description = "source code: https://github.com/AmanoTeam/unalix-web-fastapi."
 
-app_version = "2.0"
+app_version_one = "2.0"
+
+app_version_two = float(app_version_one)
+
+app_version = str(app_version_two)
 
 log_format = "<green>{time:HH:mm:ss}</green> | {level} | <level>{message}</level>"
 
